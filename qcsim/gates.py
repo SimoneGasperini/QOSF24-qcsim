2bf2153f5d5c92eb99cdc32d69a7af53e101659a
SimoneGasperini <simone.gasperini4@unibo.it>
2024-11-13 11:34:51 +0100

    Working on sympy-based tensor-mul symbolic simulation

--- qcsim/gates.py
+++ qcsim/gates.py
@@ -1,52 +1,37 @@
-import functools
-import numpy as np
-
-
-class I:
-    num_qubits = 1
-    _matrix = np.array([[1, 0],
-                        [0, 1]])
-
-    def __init__(self, qubit):
-        self.qubit = qubit
-
-    @property
-    def matrix(self):
-        return self._matrix
+import sympy
+import numpy
 
 
 class X:
     num_qubits = 1
-    _matrix = np.array([[0, 1],
-                        [1, 0]])
+    matrix = numpy.array([[0, 1],
+                          [1, 0]])
 
     def __init__(self, qubit):
         self.qubit = qubit
 
-    @property
-    def matrix(self):
-        return self._matrix
+    def to_numpy(self):
+        return numpy.array(self.matrix, dtype=complex)
 
 
 class H:
     num_qubits = 1
-    _matrix = 1/np.sqrt(2) * np.array([[1, 1],
-                                       [1, -1]])
+    matrix = 1/numpy.sqrt(2) * numpy.array([[1, 1],
+                                            [1, -1]])
 
     def __init__(self, qubit):
         self.qubit = qubit
 
-    @property
-    def matrix(self):
-        return self._matrix
+    def to_numpy(self):
+        return numpy.array(self.matrix, dtype=complex)
 
 
 class CNOT:
     num_qubits = 2
-    _matrix = np.array([[1, 0, 0, 0],
-                        [0, 1, 0, 0],
-                        [0, 0, 0, 1],
-                        [0, 0, 1, 0]])
+    matrix = numpy.array([[1, 0, 0, 0],
+                          [0, 1, 0, 0],
+                          [0, 0, 0, 1],
+                          [0, 0, 1, 0]])
 
     def __init__(self, control, target):
         self.control = control
@@ -58,20 +43,5 @@ class CNOT:
     def _max(self):
         return max(self.control, self.target)
 
-    @property
-    def matrix(self):  # as |0><0|⊗I + |1><1|⊗X
-        nqubits = self._max() - self._min() + 1
-        ctrl = self.control - self._min()
-        targ = self.target - self._min()
-        # compute first term
-        term0 = [I._matrix] * nqubits
-        term0[ctrl] = np.array([[1, 0],
-                                [0, 0]])
-        term0 = functools.reduce(np.kron, term0[::-1])
-        # compute second term
-        term1 = [I._matrix] * nqubits
-        term1[ctrl] = np.array([[0, 0],
-                                [0, 1]])
-        term1[targ] = X._matrix
-        term1 = functools.reduce(np.kron, term1[::-1])
-        return term0 + term1
+    def to_numpy(self):
+        return numpy.array(self.matrix, dtype=complex)
--- qcsim/simulator.py
+++ qcsim/simulator.py
@@ -1,89 +1,42 @@
-import functools
-import collections
-import random
-import numpy as np
-from qcsim.gates import I
+import sympy
+import numpy
 
 
 class Simulator:
 
-    def __init__(self, method):
-        # method: "matrix-mul" or "tensor-mul"
-        self.method = method
-
-    def _init_statevector(self, num_qubits):
-        state = np.zeros(2**num_qubits)
-        state[0] = 1
-        self._statevector = state
+    def __init__(self):
+        pass
 
     def _init_statetensor(self, num_qubits):
-        state = np.zeros((2,) * num_qubits)
+        state = numpy.zeros((2,) * num_qubits)
         state[(0,) * num_qubits] = 1
         self._statetensor = state
-
-    def _run_matrix_mul(self, circuit):
-        # 1) Naive simulation using matrix multiplication
-        self._init_statevector(circuit.num_qubits)
-        for layer in circuit._data:
-            matrices = [gate.matrix for gate in layer[::-1]
-                        if gate is not None]
-            operator = functools.reduce(np.kron, matrices)
-            self._statevector = operator @ self._statevector
+        # self._statetensor = sympy.Array(state)
 
     def _apply_1q_gate(self, gate):
-        gate_tensor = gate._matrix
+        gate_tensor = gate.matrix
         i = -gate.qubit - 1
-        self._statetensor = np.tensordot(
+        self._statetensor = numpy.tensordot(
             gate_tensor, self._statetensor, axes=(1, i))
-        self._statetensor = np.moveaxis(self._statetensor, 0, i)
+        self._statetensor = numpy.moveaxis(self._statetensor, 0, i)
 
     def _apply_2q_gate(self, gate):
-        gate_tensor = gate._matrix.reshape(2, 2, 2, 2)
+        gate_tensor = gate.matrix.reshape(2, 2, 2, 2)
         c = -gate.control - 1
         t = -gate.target - 1
-        self._statetensor = np.tensordot(
+        self._statetensor = numpy.tensordot(
             gate_tensor, self._statetensor, axes=((2, 3), (c, t)))
-        self._statetensor = np.moveaxis(self._statetensor, (0, 1), (c, t))
+        self._statetensor = numpy.moveaxis(self._statetensor, (0, 1), (c, t))
 
-    def _run_tensor_mul(self, circuit):
-        # 2) Advanced simulation using tensor multiplication
+    def run(self, circuit):
         self._init_statetensor(circuit.num_qubits)
-        gates = [gate for layer in circuit._data for gate in layer
-                 if gate is not None and not isinstance(gate, I)]
-        for gate in gates:
+        for gate in circuit._data:
             if gate.num_qubits == 1:
                 self._apply_1q_gate(gate)
             elif gate.num_qubits == 2:
                 self._apply_2q_gate(gate)
 
-    def run(self, circuit):
-        if self.method == 'matrix-mul':
-            self._run_matrix_mul(circuit)
-        elif self.method == 'tensor-mul':
-            self._run_tensor_mul(circuit)
-
     def get_statevector(self):
-        if self.method == 'matrix-mul':
-            psi = self._statevector
-        elif self.method == 'tensor-mul':
-            psi = self._statetensor.flatten()
+        num_qubits = len(self._statetensor.shape)
+        psi = self._statetensor.reshape(2**num_qubits)
         return psi
-
-    def sample(self, num_shots):
-        # 3) Bonus question: sampling statevector
-        psi = self.get_statevector()
-        num_qubits = int(np.log2(len(psi)))
-        states = [np.binary_repr(i, width=num_qubits)
-                  for i in range(2**num_qubits)]
-        probs = np.square(np.abs(psi))
-        samples = random.choices(states, weights=probs, k=num_shots)
-        return dict(collections.Counter(samples))
-
-    def expval(self, observable):
-        # 3) Bonus question: computing exact expectation values
-        if not np.all(observable == observable.conj().T):  # hermitian matrix
-            error_message = 'The given observable must be an Hermitian matrix!'
-            raise ValueError(error_message)
-        ket = self.get_statevector()
-        bra = ket.conj().T
-        return float(bra @ observable @ ket)
